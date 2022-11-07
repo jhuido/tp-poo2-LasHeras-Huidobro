a8b16f4f2371c21187557d0a300c147743820642
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-07 18:55:16 -0300

    'TDD_iteration'

--- OneDrive - UCA/JOSEFINA/3 AÑO/2 CUATRIMESTRE/POO II/tp-LasHeras-Huidobro/test/sistEnvio.test.js
+++ OneDrive - UCA/JOSEFINA/3 AÑO/2 CUATRIMESTRE/POO II/tp-LasHeras-Huidobro/test/sistEnvio.test.js
@@ -4,3 +4,9 @@ test("Crear Local",()=>{
     var local1=new Local("Local");
     expect(local1.nombre).toBe("Local");
 })
+
+test("Local genere Paquete",()=>{
+    var local1=new Local("Local");
+    var paquete=local1.generarPaquete();
+    expect(paquete.ubicacion).toBe("Local");
+})
